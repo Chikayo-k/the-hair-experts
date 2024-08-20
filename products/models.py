70befe52d515b00f976a8da932f161d7b64e9072
Chikayo Kambara <chika000chika@gmail.com>
2024-08-20 09:43:54 +0000

    update plural name in Admin panel

--- products/models.py
+++ products/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 # Category Model
 class Category(models.Model):
+    
+    class Meta:
+        verbose_name_plural ='Categories'
+    
     name = models.CharField(max_length=150)
     display_name = models.CharField(max_length=150, null=True, blank=True)
 
