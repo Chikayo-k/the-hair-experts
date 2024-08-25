16ef81478fcd9c489e4d4978a09b66aa5eec7f13
Chikayo Kambara <chika000chika@gmail.com>
2024-08-25 20:23:42 +0000

    Add sorting functionality

--- products/views.py
+++ products/views.py
@@ -29,10 +29,11 @@ def all_products(request):
             if sortkeyword  == 'name':
                sortkeyword  = 'lower_name'
                products = products.annotate(lower_name=Lower('name'))
-            
+            if sortkeyword == 'category':
+                sortkeyword='category__name'            
             if 'direction' in request.GET:
                 direction = request.GET['direction']
-                if direction == 'decec':
+                if direction == 'desc':
                     sortkeyword  = f'-{sortkeyword }'
             products = products.order_by(sortkeyword )
 
