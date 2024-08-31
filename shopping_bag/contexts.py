63ecf0a5dfbb0297a6ca4775bb93fe534572167a
Chikayo Kambara <chika000chika@gmail.com>
2024-08-31 09:23:27 +0000

    Add context processor

--- shopping_bag/contexts.py
+++ shopping_bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def shopping_bag_contents(request):
 
@@ -7,6 +9,21 @@ def shopping_bag_contents(request):
     total= 0
     product_count= 0
     free_delivery_price = settings.FREE_DELIVERY_PRICE
+    free_delivery_cost = 50
+    shopping_bag = request.session.get('shopping_bag', {})
+    
+
+    for item_id, quantity in shopping_bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price 
+        product_count += quantity
+        bag_items.append({
+            'item_id':item_id,
+            'quantity':quantity,
+            'product':product,
+        }) 
+
+
 
     if total >= settings.FREE_DELIVERY_PRICE:
         delivery_cost = 0
