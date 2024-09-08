f83586a052fe08eb6ff4fb1985c7a57ab0a08740
Chikayo Kambara <chika000chika@gmail.com>
2024-09-08 08:40:19 +0000

    Update chekout model and did migration

--- checkout/models.py
+++ checkout/models.py
@@ -23,13 +23,57 @@ class Order(models.Model):
     deliverly_fee = models.DecimalField(max_digits=4, decimal_places=2, null=False, default=0)
     total_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+
+    def create_order_number(self):
+        """
+        Generate order number using UUID
+        """
+        return uuid.uuid4.hex.upper()
+
+    def culc_total(self):
+        """
+        Culc grand total. accounting for deliverly cost
+        """
+        self.total_cost = self.lineitems.aggregate(Sum('item_total'))['item_total_sum']
+        if self.total_cost > settings.FREE_DELIVERY_PRICE:
+            self.deliverly_fee = settings.STANDARD_DELIVERY_PRICE
+        else:
+            self.deliverly_fee = 0
+        self.grand_total = self.total_cost + self.deliverly_fee
+        self.save()
+
+
+    def save(self, *args, **kwargs):
+        """
+        If it has not been set, override the original save method to set the order number
+        
+        """
+        if not self.order_number:
+            self.order_number = self.create_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
    
 
 
 
-class OrderItem(models.model):
+class OrderItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=True)
     item_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    
+    def save(self, *args, **kwargs):
+        """
+        update the total cost
+        
+        """
+        if not self.order_number:
+            self.item_total = self.product.price* self.quantity
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f'SKU{self.product.sku} on order {self.order_number}'
 
