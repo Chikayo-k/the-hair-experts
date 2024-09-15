3efd0cb1b05ca08ae2f4ef38ece294aee31300e7
Chikayo Kambara <chika000chika@gmail.com>
2024-09-15 14:47:38 +0000

    Add loading overlay

--- checkout/static/checkout/js/stripe.js
+++ checkout/static/checkout/js/stripe.js
@@ -39,6 +39,9 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
+
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -48,6 +51,8 @@ form.addEventListener('submit', function(ev) {
             const errorMessage = document.getElementById('card-errors');
             const content =`<p>${result.error.message}</p>`;
             $(errorMessage).html(content);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {
