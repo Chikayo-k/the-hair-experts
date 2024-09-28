6833598076cf3b01657494cae58cb0263603bd91
Chikayo Kambara <chika000chika@gmail.com>
2024-09-28 22:22:29 +0000

    Add news model and add data in the database

--- news/models.py
+++ news/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=30, unique=True)
+    body = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+       return f'News: {self.title}'
+
