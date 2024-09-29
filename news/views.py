d975739bfbcc84cce00b57432241c47b786aa3ec
Chikayo Kambara <chika000chika@gmail.com>
2024-09-29 13:32:21 +0000

    Create add function of news page

--- news/views.py
+++ news/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import News
+from news.forms import NewsForm
 
 # Create your views here.
 
 def news(request):
     news = News.objects.all()
 
-    return render(request, 'news/news.html',{'news': news})
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('news')
+    else:
+        form = NewsForm()
+            
+
+    return render(request, 'news/news.html',{'news': news, 'form':form})
