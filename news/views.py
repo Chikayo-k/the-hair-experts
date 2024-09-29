427a8900585bbab0fb42fad16f36d98457343927
Chikayo Kambara <chika000chika@gmail.com>
2024-09-29 14:50:51 +0000

    Add success and error message of form submission

--- news/views.py
+++ news/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render, redirect
 from .models import News
 from news.forms import NewsForm
+from django.contrib import messages
 
 # Create your views here.
 
 def news(request):
+    """
+    Display news
+    """
     news = News.objects.all()
-
+    
+    # Add news
     if request.method == 'POST':
+        
         form = NewsForm(request.POST)
 
         if form.is_valid():
             form.save()
+            messages.success(request, 'News was added succssfuly')
             return redirect('news')
+        else:
+            messages.error(request, 'News was not added. Please try again')
+
     else:
         form = NewsForm()
             
 
     return render(request, 'news/news.html',{'news': news, 'form':form})
+
