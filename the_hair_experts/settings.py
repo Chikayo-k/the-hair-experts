a3135f93cb14ced5cd085f639205bfe3a5e3cc49
Chikayo Kambara <chika000chika@gmail.com>
2024-10-07 20:36:11 +0000

    Deployment

--- the_hair_experts/settings.py
+++ the_hair_experts/settings.py
@@ -9,14 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 from pathlib import Path
 import dj_database_url
 import os
 from dotenv import load_dotenv
- 
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent 
 
 if os.path.isfile('env.py'):
     import env
@@ -126,16 +125,19 @@ WSGI_APPLICATION = 'the_hair_experts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
+if 'DARABASE_URL'in os.environ:
+   DATABASES = {
      'default': dj_database_url.parse(os.getenv('DATABASE'))
-}
+   }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
