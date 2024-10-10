e378738a53fabc22d9597b82f0f74f486f265c3d
Chikayo Kambara <chika000chika@gmail.com>
2024-10-10 20:29:52 +0000

    update requirements for deployment

--- the_hair_experts/settings.py
+++ the_hair_experts/settings.py
@@ -190,10 +190,10 @@ if 'USE_AWS' in os.environ:
     }
 
     #Bucket config
-    AWS_STORAGE_BUCKET_NAME ='the_hair_experts'
-    AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY =os.getenv(' AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME ='the_hair_experts'
+    AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
