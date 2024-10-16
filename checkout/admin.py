fad334de5e4e16e016a976956bf55afc350682de
Chikayo Kambara <chika000chika@gmail.com>
2024-10-16 20:00:46 +0000

    fixed testing error admin.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -3,20 +3,26 @@ from .models import Order, OrderItem
 
 # Register your models here.
 
+
 class OrdertotalAdmin(admin.TabularInline):
     model = OrderItem
-    readonly_fields=('item_total',)
+    readonly_fields = ('item_total', )
 
-class OrderAdmin(admin.ModelAdmin):    
+
+class OrderAdmin(admin.ModelAdmin):
     inlines = (OrdertotalAdmin,)
 
-    readonly_fields =('order_number','date','deliverly_fee','total_cost','grand_total','original_bag','stripe_pid')
+    readonly_fields = ('order_number', 'date', 'deliverly_fee', 
+                       'total_cost', 'grand_total', 'original_bag', 'stripe_pid')
     
-    fields = ('order_number','user_profile','date','full_name','phone','eircode','country','address1','address2', 'town_city','county_region','deliverly_fee','total_cost','grand_total','original_bag','stripe_pid')
+    fields = ('order_number', 'user_profile', 'date', 
+              'full_name', 'phone', 'eircode', 'country', 
+              'address1', 'address2', 'town_city', 'county_region', 
+              'deliverly_fee','total_cost','grand_total','original_bag','stripe_pid')
 
-    list_display=('order_number','date','full_name','total_cost','deliverly_fee','grand_total')
+    list_display = ('order_number', 'date', 'full_name', 'total_cost', 'deliverly_fee', 'grand_total')
 
-    ordering= ('-date',)
+    ordering = ('-date', )
 
 
 admin.site.register(Order,OrderAdmin)
