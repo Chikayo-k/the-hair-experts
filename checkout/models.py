17ccdd3f0cd11f256abb23b40cf634dcf057b340
Chikayo Kambara <chika000chika@gmail.com>
2024-10-16 20:50:24 +0000

    Fixed python error models.py

--- checkout/models.py
+++ checkout/models.py
@@ -13,23 +13,28 @@ from django_countries.fields import CountryField
 class Order(models.Model):
 
     order_number = models.CharField(max_length=35, null=False, editable=False)
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, 
-                                     null=True, blank=True, related_name='orders')
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=100, null=False, blank=False)
     phone = models.CharField(max_length=20, null=True, blank=True)
-    eircode = models.CharField(max_length=15,null=True, blank=True )
+    eircode = models.CharField(max_length=15, null=True, blank=True)
     country = CountryField(blank_label='Country *', null=False, blank=False)
-    address1 = models.CharField(max_length=50,null=False, blank=False)
-    address2 = models.CharField(max_length=50,null=True, blank=True)
-    town_city = models.CharField(max_length=50,null=False, blank=False)
-    county_region = models.CharField(max_length=50,null=True, blank=True)
+    address1 = models.CharField(max_length=50, null=False, blank=False)
+    address2 = models.CharField(max_length=50, null=True, blank=True)
+    town_city = models.CharField(max_length=50, null=False, blank=False)
+    county_region = models.CharField(max_length=50, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    deliverly_fee = models.DecimalField(max_digits=4, decimal_places=2, null=False, default=0)
-    total_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    deliverly_fee = models.DecimalField(max_digits=4, decimal_places=2,
+                                        null=False, default=0)
+    total_cost = models.DecimalField(max_digits=6, decimal_places=2,
+                                     null=False, default=0)
+    grand_total = models.DecimalField(max_digits=6, decimal_places=2,
+                                      null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
-    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False,
+                                  default='')
 
     def create_order_number(self):
         """
@@ -37,12 +42,11 @@ class Order(models.Model):
         """
         return uuid.uuid4().hex.upper()
 
-
     def culc_total(self):
         """
         Culc grand total. accounting for deliverly cost
         """
-        self.total_cost = self.lineitems.aggregate(Sum('item_total'))['item_total__sum'] or 0
+        self.total_cost = self.lineitems.aggregate(Sum('item_total'))['item_total__sum'] or 0  # noqa
         if self.total_cost < settings.FREE_DELIVERY_PRICE:
             self.deliverly_fee = settings.STANDARD_DELIVERY_PRICE
         else:
@@ -50,11 +54,10 @@ class Order(models.Model):
         self.grand_total = self.total_cost + self.deliverly_fee
         self.save()
 
-
     def save(self, *args, **kwargs):
         """
-        If it has not been set, override the original save method to set the order number
-        
+        If it has not been set,
+        override the original save method to set the order number
         """
         if not self.order_number:
             self.order_number = self.create_order_number()
@@ -62,25 +65,25 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_number
-   
-
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    quantity = models.IntegerField(null=False, blank=False, default=True)
-    item_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    order = models.ForeignKey(Order, null=False, blank=False,
+                              on_delete=models.CASCADE,
+                              related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False,
+                                on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False,
+                                   default=True)
+    item_total = models.DecimalField(max_digits=6, decimal_places=2,
+                                     null=False, blank=False, editable=False)
 
-    
     def save(self, *args, **kwargs):
         """
         update the total cost
-        
         """
         self.item_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
-    
-    def __str__(self):
-      return f'SKU {self.product.sku} on order {self.order.order_number}'
 
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
