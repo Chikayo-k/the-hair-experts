9799896a6d99ac4e7c9465cf2ded88502e524af0
Chikayo Kambara <chika000chika@gmail.com>
2024-10-16 22:42:19 +0000

    Fixed python test error in forms.py

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,26 +1,27 @@
 from django import forms
 from .models import UserProfile
 
+
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        exclude =('user',)
-    
+        exclude = ('user',)
+
     def __init__(self, *args, **kwargs):
         """
-        Insert placeholders and apply classes while removing automatically generated content.
-
+        Insert placeholders and apply classes
+        while removing automatically generated content.
         place labels and make first field autofocus
         """
-        super().__init__(*args,**kwargs)
-        placeholders ={
-            'default_phone':' Phone',
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone': ' Phone',
             'default_address1': 'Address 1',
             'default_address2': 'Address 2',
             'default_town_city': 'Town / City',
             'default_county_region': 'County /Region',
-            'default_eircode':'Eir Code',
-            'default_country':'Country',
+            'default_eircode': 'Eir Code',
+            'default_country': 'Country',
         }
 
         self.fields['default_phone'].widget.attrs['autofocus'] = True
@@ -31,5 +32,5 @@ class UserProfileForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input' # noqa
             self.fields[field].label = False
