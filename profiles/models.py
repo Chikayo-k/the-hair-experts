6374e1d0c32de8825f30d0457bed0d06f6c129b2
Chikayo Kambara <chika000chika@gmail.com>
2024-10-16 22:31:36 +0000

    Fixed python test error in models.py

--- profiles/models.py
+++ profiles/models.py
@@ -5,22 +5,24 @@ from django.dispatch import receiver
 
 from django_countries.fields import CountryField
 
+
 class UserProfile(models.Model):
     """
     For User profile (delivery info and order history)
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone = models.CharField(max_length=20, null=True, blank=True)
-    default_eircode = models.CharField(max_length=15,null=True, blank=True )
-    default_address1 = models.CharField(max_length=50,null=True, blank=True)
-    default_address2 = models.CharField(max_length=50,null=True, blank=True)
-    default_town_city = models.CharField(max_length=50,null=True, blank=True)
-    default_county_region = models.CharField(max_length=50,null=True, blank=True)
-    default_country = CountryField(blank_label='Country', null=True, blank=True)
+    default_eircode = models.CharField(max_length=15, null=True, blank=True)
+    default_address1 = models.CharField(max_length=50, null=True, blank=True)
+    default_address2 = models.CharField(max_length=50, null=True, blank=True)
+    default_town_city = models.CharField(max_length=50, null=True, blank=True)
+    default_county_region = models.CharField(max_length=50,
+                                             null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True,
+                                   blank=True)
 
     def __str__(self):
-            return self.user.username
-
+        return self.user.username
 
     @receiver(post_save, sender=User)
     def create_or_update_user_profile(sender, instance, created, **kwargs):
@@ -29,5 +31,5 @@ class UserProfile(models.Model):
         """
         if created:
             UserProfile.objects.create(user=instance)
-        # Existing users: just save the profile 
+        # Existing users: just save the profile
         instance.userprofile.save()
