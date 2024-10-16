3030771a0c929ef5327a6c7b4648d95cdaf2bb0d
Chikayo Kambara <chika000chika@gmail.com>
2024-10-16 22:00:18 +0000

    Fixed python test error in views.py

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import *
+from .models import Recommendation
 
 from products.models import Product
 
 # Create your views here.
 
+
 def view_bag(request):
   """A view that renders the bag contents page"""
+
   recommendation = Recommendation.objects.all()
-  context = {
-        'recommendation': recommendation
-        }
-  return render(request, 'shopping_bag/shopping_bag.html',context)
+  context = {'recommendation': recommendation}
     
-  return render(request, 'shopping_bag/shopping_bag.html')
-      
+  return render(request, 'shopping_bag/shopping_bag.html', context)
+
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
@@ -28,7 +27,7 @@ def add_to_bag(request, item_id):
 
     if item_id in list(shopping_bag.keys()):
         shopping_bag[item_id] += quantity
-        messages.success(request, f'{quantity} more {product.name} was added to your bag')
+        messages.success(request, f'{quantity} more {product.name} was added to your bag') # noqa
     else:
         shopping_bag[item_id] = quantity
         messages.success(request, f'{product.name} was added to your bag')
@@ -37,9 +36,9 @@ def add_to_bag(request, item_id):
 
     return redirect(redirect_url)
 
+
 def adjust_bag(request, item_id):
     """ Adjust bag """
-
     quantity = int(request.POST.get('quantity'))
     quantity = int(request.POST.get('quantity'))
     shopping_bag = request.session.get('shopping_bag', {})
@@ -47,7 +46,7 @@ def adjust_bag(request, item_id):
 
     if quantity > 0:
         shopping_bag[item_id] = quantity
-        messages.success(request, f'{product.name} quantity: {quantity} in your bag')
+        messages.success(request, f'{product.name} quantity: {quantity} in your bag') # noqa
     else:
         shopping_bag.pop(item_id)
         messages.success(request, f'{product.name} was removed form your bag')
@@ -67,8 +66,5 @@ def remove_bag(request, item_id):
         return HttpResponse(status=200)
 
     except Exception as e:
-        message.error(request, f'Error: {e}')
+        messages.error(request, f'Error: {e}')
         return HttpResponse(status=500)
-
-
-
