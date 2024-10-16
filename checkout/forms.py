e353b5df9ac756ce640f52c101365fa316a8cb4a
Chikayo Kambara <chika000chika@gmail.com>
2024-10-16 20:17:57 +0000

    Fixed python test error forms.py

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,27 +1,30 @@
 from django import forms
 from .models import Order
 
+
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('full_name','email','phone','address1','address2','town_city','county_region','country','eircode',)
-    
+        fields = ('full_name', 'email', 'phone', 'address1',
+                  'address2', 'town_city', 'county_region',
+                  'country', 'eircode', )
+
     def __init__(self, *args, **kwargs):
         """
-        Insert placeholders and apply classes while removing automatically generated content.
-
+        Insert placeholders and apply classes
+        while removing automatically generated content.
         place labels and make first field autofocus
         """
-        super().__init__(*args,**kwargs)
-        placeholders ={
+        super().__init__(*args, **kwargs)
+        placeholders = {
             'full_name': 'Full Name',
-            'email':' Email ',
-            'phone':' Phone',
+            'email': ' Email ',
+            'phone': ' Phone',
             'address1': 'Address 1',
             'address2': 'Address 2',
             'town_city': 'Town / City',
             'county_region': 'County /Region',
-            'eircode':'Eir Code'
+            'eircode': 'Eir Code'
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
