4f0a0d5d676eee73072af0c2b5addb82afb1a2d3
Chikayo Kambara <chika000chika@gmail.com>
2024-10-16 22:12:27 +0000

    Fixed python test error in models.py

--- shopping_bag/models.py
+++ shopping_bag/models.py
@@ -4,11 +4,13 @@ from products.models import Product
 
 # Create your models here.
 
+
 class Recommendation(models.Model):
-   product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
-   recommendation_title = models.CharField(max_length=30)
-   description = models.TextField()
-   exclusive = models.CharField(max_length=30)
-   
-   def __str__(self):
-     return f"Recommendation for {self.product}"
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,
+                                related_name='product')
+    recommendation_title = models.CharField(max_length=30)
+    description = models.TextField()
+    exclusive = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"Recommendation for {self.product}"
