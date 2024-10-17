7ac8a9865efcf1980a3b1bb3ef71e6f5e27be204
Chikayo Kambara <chika000chika@gmail.com>
2024-10-17 20:50:59 +0000

    Fixed python test error in models.py

--- products/models.py
+++ products/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 
 # Category Model
 class Category(models.Model):
-    
+
     class Meta:
-        verbose_name_plural ='Categories'
-    
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=150)
     display_name = models.CharField(max_length=150, null=True, blank=True)
 
@@ -16,25 +16,29 @@ class Category(models.Model):
 
     def get_display_name(self):
         return self.display_name
-    
+
 
 # Product Model
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True,
+                                 blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=100, null=True, blank=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
-    image = models.ImageField (null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=1,
+                                 null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
 
-#Reviews for products
+
+# Reviews for products
 class Comment(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='chosen_product')  
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')  
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='chosen_product') # noqa  
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='commenter')
     review_title = models.CharField(max_length=20)
     comment_area = models.TextField()
     comment_date = models.DateTimeField(auto_now_add=True)
