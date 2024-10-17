878bf561fb795ff1c723249bf0d8917f5e13be30
Chikayo Kambara <chika000chika@gmail.com>
2024-10-17 20:45:48 +0000

    Fixed python test error in urls.py

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('add/', views.add_item, name='add_item'),
     path('edit/<int:product_id>/', views.edit_item, name='edit_item'),
     path('delete/<int:product_id>/', views.delete_item, name='delete_item'),
-    path('<int:product_id>/delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
+    path('<int:product_id>/delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'), # noqa
 ]
