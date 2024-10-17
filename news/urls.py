39b7cfada491f946380b06cbefdaddf0aa23e24e
Chikayo Kambara <chika000chika@gmail.com>
2024-10-17 21:18:49 +0000

    Fixed python test error in urls.py

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
     path('', views.news, name='news'),
-    path('news_add/',views.news_add, name='news_add'),
-    path('news_edit/<int:news_id>',views.news_edit, name='news_edit'),
+    path('news_add/', views.news_add, name='news_add'),
+    path('news_edit/<int:news_id>', views.news_edit, name='news_edit'),
     path('news_delete/<int:news_id>', views.news_delete, name='news_delete'),
 ]
