aa606e9dd3aa87ca1dd97e962453357420c4215b
Chikayo Kambara <chika000chika@gmail.com>
2024-10-18 11:56:00 +0000

    Add code to remove jquery error for testing

--- profiles/static/profiles/js/countryfield.js
+++ profiles/static/profiles/js/countryfield.js
@@ -1,7 +1,8 @@
+/*globals $:false */
 let countrySelected = $('#id_default_country').val();
 if(!countrySelected) {
     $('#id_default_country').css('color', 'gray');
-};
+}
 $('#id_default_country').change(function() {
     countrySelected = $(this).val();
     if(!countrySelected) {
