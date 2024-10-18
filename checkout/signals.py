148aa2c3f98ea7520ce59b771ae896a9f6df6d03
Chikayo Kambara <chika000chika@gmail.com>
2024-10-18 09:43:58 +0000

    Fixed python test errors

--- checkout/signals.py
+++ checkout/signals.py
@@ -9,13 +9,12 @@ def update_on_save(sender, instance, created, **kwargs):
     """
     Update order total (update/create)
     """
-    # instance.order.calc_total()
     instance.order.culc_total()
+
 
 @receiver(post_delete, sender=OrderItem)
 def update_on_delete(sender, instance, **kwargs):
     """
     Update order total (delete)
     """
-    # instance.order.calc_total()
     instance.order.culc_total()
