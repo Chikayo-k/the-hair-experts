caa417bf051551779b37131b1c08924ae6199f50
Chikayo Kambara <chika000chika@gmail.com>
2024-10-19 14:36:32 +0000

    Fixed python test errors

--- the_hair_experts/urls.py
+++ the_hair_experts/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('news/', include('news.urls')),
-    path('newsletter/', include('newsletter.urls')), 
+    path('newsletter/', include('newsletter.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'the_hair_experts.views.handler404'
